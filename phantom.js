864d9b8eaa14352533474fe3b4448c82835b50b8
Mystic Troll <75292383+nixxogee@users.noreply.github.com>
2022-08-04 11:50:42 -0400

    Create phantom.js

---
+++ phantom.js
@@ -0,0 +1,14 @@
+<script>
+
+phantomButton.addEventListener("click", getAccount);
+const isPhantomInstalled = window.phantom?.solana?.isPhantom
+
+function getAccount ()
+
+{
+    window.solona.connect
+    window.solona.request({method:"connect"})
+    
+    }
+
+</script>
